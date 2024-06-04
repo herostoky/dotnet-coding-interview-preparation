0344d5db22c85357e2da35511a3734a5671115d6
Toky SOLOFONIRINA <herostoky@gmail.com>
2024-06-04 19:35:31 +0000

    Update:updates-v24.tar.gz

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿using CodingTestPreparation.Puzzle;
-var result = Temperature.ComputeClosestToZero([1, 2, -1]);
-Console.WriteLine($"Hello, World!, Temp is {result}");
-WeightOrder.Solve(0,3,1);
-Console.WriteLine("madam is Palindrome ? :=> " + PalindromeCheck.IsPalindrome("madam"));
+
+namespace CodingTestPreparation
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = Temperature.ComputeClosestToZero([1, 2, -1]);
+            Console.WriteLine($"Hello, World!, Temp is {result}");
+            WeightOrder.Solve(0,3,1);
+            Console.WriteLine("madam is Palindrome ? :=> " + PalindromeCheck.IsPalindrome("madam"));
+        }
+    }
+}
